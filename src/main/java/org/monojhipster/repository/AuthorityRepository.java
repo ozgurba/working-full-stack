460d5b235e5ffacc9baaacaf9ecfb6d8fd898b38
ozgurba <baglioglu@yahoo.com>
2022-03-19 20:26:13 +0300

    Initial version of monojhipster generated by generator-jhipster@7.7.0

---
+++ src/main/java/org/monojhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.monojhipster.repository;
+
+import org.monojhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/org/monojhipster/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.monojhipster.service.dto;
